c231a0e0379ca74b00f58b0130266c88c4612383
Reed Dunkle <reeddunkle@gmail.com>
2020-03-13 12:09:58 -0400

    WIP: basic styling

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import "./styles.css";
 
 import skills from "./data/skills.json";
 import statusEffects from "./data/statusEffects.json";
 
-const noop = () => {};
+// const noop = () => {};
 
-const processData = (skills, statusEffects) => {
-  return { skills, statusEffects };
+const searchCures = (ailment, { skills, statusEffects }) => {
+  return skills;
+};
+
+const SearchResults = ({ results }) => {
+  return (
+    <div className="search-results">
+      {results.map((result, index) => (
+        <>
+          <SearchResult {...result} />
+          {index !== results.length - 1 && (
+            <hr className="search-results__hr" />
+          )}
+        </>
+      ))}
+    </div>
+  );
+};
+
+const SearchResult = ({ description, imageSrc, name }) => {
+  return (
+    <div className="search-result">
+      <img className="search-result__image" src={imageSrc} />
+      <span className="search-result__name">{name}</span>
+      <span className="search-result__description">{description}</span>
+    </div>
+  );
 };
 
 export default function App() {
-  const [value, setValue] = useState();
+  const [searchValue, setSearchValue] = useState();
+  const [cureResults, setCureResults] = useState([]);
 
-  const data = useMemo(() => processData(skills, statusEffects), [
-    processData,
-    skills,
-    statusEffects
-  ]);
-
-  console.log("data", data);
-
-  const handleChange = event => {
-    setValue(event.target.value);
+  const handleSearchChange = event => {
+    setSearchValue(event.target.value);
   };
+
+  useEffect(() => {
+    const cureResults = searchCures(searchValue, { skills, statusEffects });
+    setCureResults(cureResults);
+  }, [searchValue, skills, statusEffects]);
 
   return (
     <div className="window">
       <div className="app">
-        <h1 className="heading">Divinity Original Sin 2</h1>
-        <input
-          className="search"
-          placeholder="Search..."
-          onChange={handleChange}
-        />
+        <div className="app__content">
+          <h1 className="heading">Divinity Original Sin 2</h1>
+          <input
+            className="search"
+            placeholder="Search Cures..."
+            onChange={handleSearchChange}
+            value={searchValue}
+          />
+          <SearchResults results={cureResults} />
+        </div>
       </div>
     </div>
   );
