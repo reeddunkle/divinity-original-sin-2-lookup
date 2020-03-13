1b5a7ce4aa44ead0ba902f0c4427c71cbb682bbc
Reed Dunkle <reeddunkle@gmail.com>
2020-03-13 10:37:33 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import "./styles.css";
 
+import skills from "./data/skills.json";
+import statusEffects from "./data/statusEffects.json";
+
+const noop = () => {};
+
+const processData = (skills, statusEffects) => {
+  return { skills, statusEffects };
+};
+
 export default function App() {
+  const [value, setValue] = useState();
+
+  const data = useMemo(() => processData(skills, statusEffects), [
+    processData,
+    skills,
+    statusEffects
+  ]);
+
+  console.log("data", data);
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="window">
+      <div className="app">
+        <h1 className="heading">Divinity Original Sin 2</h1>
+        <input
+          className="search"
+          placeholder="Search..."
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 }
