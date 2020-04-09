5dcdfd4d3297a5d4a040366388c65e616a03a5b6
Reed Dunkle <reeddunkle@gmail.com>
2020-04-08 20:20:47 -0400

    Update areStringsSimilar

--- src/util/areStringsSimilar.spec.js
+++ src/util/areStringsSimilar.spec.js
@@ -6,43 +6,17 @@ import areStringsSimilar, {
 let actual;
 let expected;
 
-describe("isNumber", () => {
-  test("", () => {
-    actual = isNumber(1);
-    expected = true;
-    expect(actual).toBe(expected);
-
-    actual = isNumber("1");
-    expected = false;
-    expect(actual).toBe(expected);
-  });
-});
-
-describe("getShortestLength", () => {
-  test("", () => {
-    actual = getShortestLength("xy", "abc");
-    expected = 2;
-    expect(actual).toBe(expected);
-
-    actual = getShortestLength("abcd", "xyz");
-    expected = 3;
-    expect(actual).toBe(expected);
-
-    actual = getShortestLength("xyz", "abc");
-    expected = 3;
-    expect(actual).toBe(expected);
-  });
-});
-
 describe("areStringsSimilar", () => {
-  test("", () => {
+  test("similar start", () => {
     actual = areStringsSimilar(
       "The man went over to the bar",
       "The man went over to the house"
     );
     expected = true;
     expect(actual).toBe(expected);
+  });
 
+  test("similar end", () => {
     actual = areStringsSimilar(
       "The woman went over to the bar",
       "The man went over to the bar"
@@ -56,14 +30,40 @@ describe("areStringsSimilar", () => {
 
     actual = areStringsSimilar(
       "The man went over to the bar",
-      "The woman went over to the bar",
+      "The music went over to the bar",
       {
         endCount: 0,
         startCount: searchInput.length,
       }
     );
 
+    expected = true;
+    expect(actual).toBe(expected);
+  });
+});
+
+describe("[dependencies]", () => {
+  test("isNumber", () => {
+    actual = isNumber(1);
+    expected = true;
+    expect(actual).toBe(expected);
+
+    actual = isNumber("1");
     expected = false;
+    expect(actual).toBe(expected);
+  });
+
+  test("getShortestLength", () => {
+    actual = getShortestLength("xy", "abc");
+    expected = 2;
+    expect(actual).toBe(expected);
+
+    actual = getShortestLength("abcd", "xyz");
+    expected = 3;
+    expect(actual).toBe(expected);
+
+    actual = getShortestLength("xyz", "abc");
+    expected = 3;
     expect(actual).toBe(expected);
   });
 });
